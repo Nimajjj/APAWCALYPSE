0531bd1a4179a0b37264adf9f71e262d87031d44
unknown <benjamin.borello@aol.com>
2023-01-27 01:26:32 +0100

    chore(weapon); reformat

--- scripts/weapon/weapon.gd
+++ scripts/weapon/weapon.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 class_name Weapon
 
-enum Weapon_weight {LIGHT, MEDIUM, HEAVY}
+enum Weapon_Weight {LIGHT, MEDIUM, HEAVY}
 
 var weapon_name: String
 
@@ -10,9 +10,9 @@ var accuracy: int
 var damage: int
 var range: int
 var spread: int
+var fire_rate: int
 
 #var bullet_type: Bullet
-var fire_rate: int
 
 var bullet_stock: int
 var max_bullet_stock: int
@@ -21,29 +21,31 @@ var mag_capacity: int
 var reload_time: int
 
 var price: int
-var weight: Weapon_weight
+var weight: Weapon_Weight
 
-var secondary_weapon: Weapon
+var secondary_weapon: Weapon = null
 
 
 func has_secondary_weapon() -> bool:
-	if secondary_weapon != null:
-		return true
-	else:
-		return false
+	return (secondary_weapon != null)
 
+
+# /!\ Function must act on stock bullet 
 func refill(amount: int):
 	if bullet_stock + amount <= max_bullet_stock:
 		bullet_stock += amount
 	else:
 		bullet_stock = mag_capacity	
 
+
 func reload():
 	pass
 	
+
 func secondary_shoot():
 	if has_secondary_weapon():
 		secondary_weapon.shoot()
+	
 	
 func shoot():
 	pass
