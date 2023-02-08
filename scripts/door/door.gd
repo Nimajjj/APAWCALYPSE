e9ef3d81b75361db4137a91d18268e6dc2a14592
MaxYnovobry <94967857+MaxYnovobry@users.noreply.github.com>
2023-02-08 14:43:02 +0100

    door

--- scripts/door/door.gd
+++ scripts/door/door.gd
@@ -1,4 +1,4 @@
-extends Node
+extends StaticBody2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_area_2d_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body):
+	pass # Replace with function body.
