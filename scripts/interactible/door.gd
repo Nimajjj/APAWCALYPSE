b4d501947f23a326c58d59fb9fcf2d968a499404
Nimajjj <benjamin.borello@aol.com>
2023-03-01 11:19:30 +0100

    feat(interactible): Add interactile interface + door

---
+++ scripts/interactible/door.gd
@@ -0,0 +1,26 @@
+extends Interactible
+
+@export var price: int = 0
+@export var open: bool = false
+@export var spawner_id: int = -1
+
+
+func activate(player: IPlayer) -> void:
+	if can_activate(player) :
+		open = true
+		player.money -= price
+		visible	 = false
+
+
+func can_activate(player: IPlayer) -> bool:
+	return player.money >= price && !open
+
+
+func _on_area_2d_body_entered(body) -> void :
+	if body is IPlayer:
+		body.add_interactible(self)
+
+
+func _on_area_2d_body_exited(body) -> void :
+	if body is IPlayer:
+		body.remove_interactible(self)
---
+++ scripts/interactible/interactible.gd
@@ -0,0 +1,10 @@
+class_name Interactible
+extends Node2D
+
+
+func activate(player: IPlayer) -> void:
+	pass
+
+
+func can_activate(player: IPlayer) -> bool:
+	return false
