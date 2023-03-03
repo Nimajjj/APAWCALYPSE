3edda0c357973769a7b779a0f9e6cb5fe9851d8f
Nimajjj <benjamin.borello@aol.com>
2023-03-03 16:52:12 +0100

    chore(game): Add global singleton + add players array in game

--- scripts/game/game.gd
+++ scripts/game/game.gd
@@ -1,19 +1,24 @@
+class_name Game
 extends Node2D
 
 var score: int = 0
 var wave: int = 0
 var time: float = 0.0
 
+var players: Array[IPlayer] = []
+var n_players: int = 0
+
 @onready var GameTimer: Timer = $GameTimer
 @onready var FabricPlayer: Node2D = $FabricPlayer
 
 
 func _ready():
+	Global.game = self
 	start_game()
 	
 
 func start_game() -> void:
-	FabricPlayer.create_player()
+	_new_player()
 	GameTimer.start()
 
 
@@ -21,6 +26,11 @@ func end_game() -> void:
 	wave = 0
 	time = 0.0
 	score = 0
+
+
+func _new_player() -> void:
+	players.append(FabricPlayer.create_player())
+	n_players += 1
 
 
 func _on_game_timer_timeout():
