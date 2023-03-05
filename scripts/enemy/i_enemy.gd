9185b63cede93e4b2aedadccb0a3a7bd20f93241
Bahsiik <o.mist1303@gmail.com>
2023-03-05 17:20:18 +0100

    feat(enemy): IEnemy sprite now flip

--- scripts/enemy/i_enemy.gd
+++ scripts/enemy/i_enemy.gd
@@ -2,6 +2,8 @@ class_name IEnemy
 extends CharacterBody2D
 
 
+@onready var Sprite = $Sprite2D
+
 @export var max_health: int
 @export var speed: int
 @export var money: int
@@ -24,6 +26,10 @@ func take_damage(dmg: int, shooter: IPlayer) -> void:
 func _move(delta) -> void:
 	var target = get_parent().player
 	var _direction = (target.global_position - global_position).normalized()
+	if _direction.x < 0:
+		Sprite.flip_h = false
+	elif _direction.x > 0:
+		Sprite.flip_h = true
 	var _velocity = _direction * speed * delta
 	position += _velocity
 
