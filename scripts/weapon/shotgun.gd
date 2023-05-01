6d809bca4113319aff5b79a68d21d9852ab65e09
antaww <anta.pizzetta@gmail.com>
2023-05-01 14:41:53 +0200

    feat(weapons): Implement magazine on shotgun.
    Closes #75

--- scripts/weapon/shotgun.gd
+++ scripts/weapon/shotgun.gd
@@ -1,23 +1,20 @@
 extends IWeapon
 
 
-#func _ready():
-##	spread = 0.4
-#	weapon_range = 0.1
-
-
 func shoot() -> void:
+	if reloading: return
 	if(actual_rate == fire_rate):
-		for i in range(12):
-			var bullet: IBullet = BulletScene.instantiate()
-			bullet.position = WeaponEnd.get_global_transform().origin
-			bullet.damage = damage
-			bullet.life_time = weapon_range
-			get_tree().get_root().add_child(bullet)
-			var spread_angle = randf_range(-spread, spread)
-			var shoot_direction = weapon_direction.rotated(spread_angle)
-			bullet.speed -= randi() % 50
-			bullet.shoot(get_parent(), get_global_mouse_position(), shoot_direction)
-			actual_rate = 0
+		if(current_mag > 0):
+			for i in range(12):
+				var bullet: IBullet = BulletScene.instantiate()
+				bullet.position = WeaponEnd.get_global_transform().origin
+				bullet.damage = damage
+				bullet.life_time = weapon_range
+				current_mag -= 1
+				get_tree().get_root().add_child(bullet)
+				var spread_angle = randf_range(-spread, spread)
+				var shoot_direction = weapon_direction.rotated(spread_angle)
+				bullet.shoot(get_parent(), get_global_mouse_position(), shoot_direction)
+				actual_rate = 0
 	actual_rate += 1
 
